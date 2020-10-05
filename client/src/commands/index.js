5fe7d87c095c62e80553cbf7bcf0705a58ce555b
Trevor Brown <trevorbrown.dev@gmail.com>
2020-10-05 14:27:01 -0400

    test

--- client/src/commands/index.js
+++
@@ -1,9 +0,0 @@
-import ritem from './ritem';
-import renvironment from './renvironment';
-import rsurvivor from './rsurvivor';
-import rmonster from './rmonster';
-import rinteractable from './rinteractable';
-import rdrone from './rdrone';
-import rchallenge from './rchallenge';
-
-export { ritem, renvironment, rsurvivor, rmonster, rinteractable, rdrone, rchallenge };
--- server/src/requests/index.js
+++
@@ -1,9 +0,0 @@
-import getItem from './getItem';
-import getMonster from './getMonster';
-import getSurvivor from './getSurvivor.js';
-import getEnvironment from './getEnvironment.js';
-import getDrone from './getDrone.js';
-import getInteractable from './getInteractable';
-import getChallenge from './getChallenge';
-
-export { getItem, getMonster, getSurvivor, getEnvironment, getInteractable, getDrone, getChallenge };
