11237b04be3a6693022c40b9755c30b467972df1
Trevor Brown <trevorbrown.dev@gmail.com>
2020-10-09 15:22:49 -0400

    Cleaned up runlock and added link to unlock

--- src/commands/rchallenge.js
+++ src/commands/rchallenge.js
@@ -5,10 +5,11 @@ const rchallenge = (message, challengeName) => {
     RoR.get(`/challenges/${challengeName}`).then(({ data: challenge }) => {
         const embed = new MessageEmbed();
         embed.setColor('BCE8D2');
+        embed.setTitle(challenge.title);
         embed.setThumbnail(challenge.image);
-        embed.setTitle(challenge.name);
-        embed.setAuthor(`Risk of Rain 2 Wiki - ${challenge.name}`, '', `https://riskofrain2.gamepedia.com/${challenge.name.replace(/ +/g, '_')}`);
-        embed.addField('Challenge:', `${challenge.text}\n`, false);
+        embed.setDescription(challenge.description);
+        embed.setAuthor(`Risk of Rain 2 Wiki - ${challenge.title}`, '', `https://riskofrain2.gamepedia.com/${challenge.title.replace(/ +/g, '_')}`);
+        embed.addField('Unlocks', `[${challenge.unlock.value}](https://riskofrain2.gamepedia.com/${challenge.unlock.value.replace(/ +/g, '_')})`, false);
 
         message.channel.send(embed);
     });
